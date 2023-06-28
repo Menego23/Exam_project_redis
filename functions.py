2ff3b77c4f59adc28ffbb716c035510bcaf0ff94
Il Bonfo <stebon03@gmail.com>
2023-06-28 10:10:32 +0200

    Add vota_proposte()

--- functions.py
+++ functions.py
@@ -6,8 +6,6 @@ db = redis.Redis(
   password='password')
 
 
-
-
 ##############################################################################################################
 # VEDERE LE PROPOSTE
 ##############################################################################################################
@@ -17,8 +15,6 @@ def mostra_proposte():
     for i, (proposta, voti) in enumerate(proposte, start=1):
         proponenti = ', '.join(db.smembers(f"proponenti:{proposta}"))
         print(f"{i}. {proposta.decode()} ({proponenti}): {int(voti)} voti")
-
-
 
 
 ##############################################################################################################
@@ -34,6 +30,27 @@ def nuova_proposta():
     db.zadd('voti_proposte', {f"proposta:{proposta_id}": 0})
 
 
+def vota_proposta():
+    nome_studente = input("Chi sei? ")
+    mostra_proposte()
+    proposta_voto = input(
+        "Che proposta voti? (inserisci il numero corrispondente o '0' per tornare al menu principale): ")
+    if proposta_voto == '0':
+        return
+
+    proposte = db.zrevrangebyscore('voti_proposte', '+inf', '-inf', withscores=True)
+    if proposta_voto.isdigit() and 1 <= int(proposta_voto) <= len(proposte):
+        proposta = proposte[int(proposta_voto) - 1]
+        proposta_id = proposta[0].decode()
+
+        if not db.sismember(f"votanti:{proposta_id}", nome_studente):
+            db.sadd(f"votanti:{proposta_id}", nome_studente)
+            db.zincrby('voti_proposte', 1, proposta_id)
+            print("Voto registrato!")
+        else:
+            print("Hai già votato questa proposta.")
+    else:
+        print("Selezione non valida.")
 
 
 ##############################################################################################################
