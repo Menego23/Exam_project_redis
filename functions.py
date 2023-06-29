2802371c6c138c28ba01a33c287edcc3399fcc51
Il Bonfo <stebon03@gmail.com>
2023-06-29 10:11:57 +0200

    Spostamento main_function()

--- functions.py
+++ functions.py
@@ -63,3 +63,29 @@ def mostra_top_proposte():
     for i, (proposta, voti) in enumerate(proposte, start=1):
         proponenti = ', '.join(db.smembers(f"proponenti:{proposta}"))
         print(f"{i}. {proposta.decode()} ({proponenti}): {int(voti)} voti")
+
+
+if __name__ == "__main__":
+    while True:
+        print()
+        print("Scegli:")
+        print("1. Nuova proposta")
+        print("2. Vota una proposta")
+        print("3. Mostra proposte attuali")
+        print("4. Mostra top proposte")
+        print("0. Esci")
+        scelta = input()
+
+        if scelta == '1':
+            nuova_proposta()
+        elif scelta == '2':
+            vota_proposta()
+        elif scelta == '3':
+            mostra_proposte()
+        elif scelta == '4':
+            mostra_top_proposte()
+        elif scelta == '0':
+            break
+        else:
+            print("Scelta non valida.")
+
