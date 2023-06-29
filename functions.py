addd383d24a47a97709ec218a507d24df2cfdb2d
Gianluca Meneghetti <g.meneghetti2003@gmail.com>
2023-06-29 11:27:11 +0200

    Bug generali

--- functions.py
+++ functions.py
@@ -1,11 +1,13 @@
 import redis
 
-db = redis.Redis(
-  host='x',
-  port=00000,
-  password='password')
+# Connessione al database
 
+r = redis.Redis(
+  host='redis-12114.c293.eu-central-1-1.ec2.cloud.redislabs.com',
+  port=12114,
+  password='3FYHn60i42mOmITHT7CnlJI4SoYyoX4P')
 
+db = r
 ##############################################################################################################
 # VEDERE LE PROPOSTE
 ##############################################################################################################
--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from functions import *
 
+
+
 while True:
     print()
     print("Scegli:")
