f49d797d32c3bbf21e408b6ac221e0c305a0b746
Gabriele Laguna <g.laguna@itsrizzoli.it>
2023-06-30 12:31:48 +0200

    main.py for py 3.9
    il main ora funziona per python 3.9, non contiene più match (sostituita da serie di elif)

--- main.py
+++ main.py
@@ -1,32 +1,25 @@
 from functions import *
 
 while True:
-    login()
     print()
-    print("Scegli:")
+    print("Seleziona un'opzione:")
     print("1. Nuova proposta")
     print("2. Vota una proposta")
     print("3. Mostra proposte attuali")
     print("4. Mostra top proposte")
     print('5. Ricerca proposte')
     print("0. Esci")
-    scelta = input()
+    scelta = input("\ncosa vuoi fare?")
 
-    match scelta:
-        case '1':
-            nuova_proposta()
-            break
-        case '2':
-            vota_proposta()
-            break
-        case '3':
-            mostra_proposte()
-            break
-        case '4':
-            mostra_top_proposte()
-            break
-        case '5':
-            ricerca_proposte()
-            break
-        case _:
-            print("Scelta non valida.")
+    if scelta == '1':
+        nuova_proposta()
+    elif scelta == '2':
+        vota_proposta()
+    elif scelta == '3':
+        mostra_proposte()
+    elif scelta == '4':
+        mostra_top_proposte()
+    elif scelta == '5':
+        ricerca_proposte()
+    else:
+        print("Scelta non valida.")
